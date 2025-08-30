caa0faf9e61092448913f6020b15cdcf285f3121
Tashaun33 <21pnts28@gmail.com>
2025-08-30 17:36:04 +0100

    i did these things

---
+++ class-fri-22-aug-2025.py
@@ -0,0 +1,393 @@
+
+#just some note taking a few thoughts of my own , for me now though keep the focus ,beleive in better and a future
+#and maybe just maybe  i will have some satisfaction from something. The course is still good.
+
+
+
+
+
+a=('class on Friday 22nd August 2025 Ad')
+print(a)
+input('please press enter to continue')
+
+b=('14pm sharp')
+print(b)
+input('press enter')
+
+c=('where are my python skills')
+print(c)
+input('press enter')
+
+d=('remember to walk down the LMS')
+print(d)
+input('print enter')
+
+e=(' spilt the screen LMS 1 side python window on the other,from previous snips of class work')
+print(e)
+input('press enter')
+
+f=('what are the basic functions In python')
+print(f)
+input('press enter')
+
+g=('what experience do they have with computers')
+print(g)
+input('press enter')
+
+h=('recall a fuction in python')
+print(h)
+input('press enter')
+
+i=('create a check for visual aid today')
+print(i)
+input('press enter')
+
+j=('tick')
+print(j)
+input('press enter')
+
+k=('tick')
+print(k)
+input('press enter')
+
+l=('tick')
+print(l)
+input('press enter')
+
+m=('time and place')
+print(m)
+input('press enter')
+
+n=('tick')
+print(n)
+input('press enter')
+
+o=('tick')
+print(o)
+input('press enter')
+
+p=('experience pays')
+print(p)
+input('press enter')
+
+
+q=('zodiac- today-tick')
+print(q)
+input('press enter')
+
+
+r=('first friday- today tick')
+print(r)
+input('press enter')
+
+
+s=('walk last zodiac to exercise in city park- tick')
+print(s)
+input('press enter')
+
+t=('school days school gate- tick')
+print(t)
+input('press enter')
+
+
+u=('green adidas tick')
+print(u)
+input('press enter')
+
+
+v=('work-life balance- thought check-tick')
+print(v)
+input('press enter')
+
+w=('user experience class material')
+print(w)
+input('press enter')
+
+
+x=('not asking us tobe graphic designers')
+print(x)
+input('press enter')
+
+
+y=('in employment the employer may prodide you with data set, ideas storyboards')
+print(y)
+input('press enter')
+
+
+z=('code institute recommend using gen AI for storybording.')
+print(z)
+input('press enter')
+
+
+aa=('user story through GEN AI')
+print(aa)
+input('press enter')
+
+
+bb=('NO manager')
+print(bb)
+input('press enter')
+
+
+cc=('no engineers')
+print(cc)
+input('press enter')
+
+
+dd=('No developer')
+print(dd)
+input('press enter')
+
+
+ee= ('we as students are alone on the Bootcamp course')
+print(ee)
+input('press enter')
+
+ff=('make users with GEN AI about web page ive created from code inst teaching')
+print(ff)
+input('press enter')
+
+
+gg=('get help from GEN AI to test your own DATA')
+print(gg)
+input('press enter')
+
+
+hh=('Niel McEwen')
+print(hh)
+input('press enter')
+
+
+ii=('3:01 PM')
+print(ii)
+input('press enter')
+
+
+
+jj=('https://docs.google.com/document/d/e/2PACX-1vR9RUFwdv2LLNBkez7NK5xdtQmiXN5CZ7HNVloSydu_k88l9ZuoaElrWkfegCuTFnHrch4gbcDCWonk/pub')
+print(jj)
+input('press enter')
+
+
+kk=('is GEN-AI the FUTURE')
+print(kk)
+input('press enter')
+
+
+ll=('the pentagram Framework')
+print(ll)
+input('press enter')
+
+
+mm=('one way of doing business goals and user storys')
+print(mm)
+input('press enter')
+
+
+
+nn=('haircut cat video tick')
+print(mm)
+input('press enter')
+
+oo=('classroom 2019 -tick')
+print(oo)
+input('press enter')
+
+
+
+pp=('kidderminister interview -tick')
+print(pp)
+input('press enter')
+
+
+qq=('delivery ps3 driver-golden tick')
+print(qq)
+input('press enter')
+
+rr=('stream and candle 2020 oct -tick')
+print(rr)
+input('press enter')
+
+
+ss=('maybe I remember more so I can be more-')
+print(ss)
+input('press enter')
+
+
+tt=('2020-2023 AD workplace windows and doors--tick')
+print(tt)
+input('press enter')
+
+
+uu=('how much GEN AI in the future????')
+print(uu)
+input('press enter')
+
+vv=('hair up in the living room 2016- tick')
+print(vv)
+input('press enter')
+
+
+ww=('where will I be in 10 years from now')
+print(ww)
+input('press enter')
+
+
+xx=('can I see where im coming from to where I might be-???')
+print(xx)
+input('press enter')
+
+yy=('teacher Niel knows a lot...')
+print(yy)
+input('press enter')
+
+zz=('great')
+print(zz)
+input('press enter')
+
+
+aaa=('vid with hair in class 2019 tick')
+print(aaa)
+input('press enter')
+
+
+bbb=('vid of west park sept 2020 tick')
+print(bbb)
+input('press enter')
+
+
+ccc=('practice is the way to get thing s fixed in the head-- quote of Teacher Niel- bless')
+print(ccc)
+input('press enter')
+
+
+
+ddd=('vid of backyard 2016/2015 -tick')
+print(ddd)
+input('press enter')
+
+
+eee=('DATA-- KATA Kata from martial arts practice python list 4 or 5 times then delete then do it without looking at text a bit like muscle memory..')
+print(eee)
+input('press enter')
+
+
+fff=('practice WILL MAKE PERFECT')
+print(fff)
+input('press enter')
+
+
+ggg=('STYLE INTERleaving 16 WEEK BOOTCAMP')
+print(ggg)
+input('press enter')
+
+
+hhh=('takes in Power BI similar to LMS in python')
+print(hhh)
+input('press enter')
+
+
+iii=('cassette pack--tick')
+print(iii)
+input('press enter')
+
+
+jjj=('deorderant-- tick')
+print(jjj)
+input('press enter')
+
+
+
+kkk1=('raby street earth soil--tick')
+print(kkk1)
+input('press enter')
+
+
+lll=('orange tshirt wales 2019--tick')
+print(lll)
+input('press enter')
+
+
+mmm=('morechidai--tick')
+print(mmm)
+input('press enter')
+
+
+nnn=('salamanders --tick')
+print(nnn)
+input('press enter')
+
+
+ooo=('our brains are not that great at remember things learned 1 day, forgot the following week----')
+print(ooo)
+input('press enter')
+
+
+ppp=('blue pearl=--- tick')
+print(ppp)
+input('press enter')
+
+qqq=('sega master system- tick')
+print(qqq)
+input('press enter')
+
+
+rrr=('pound coin 100% gold ---tick')
+print(rrr)
+input('press enter')
+
+
+sss=('syntax in excel')
+print(sss)
+input('press enter')
+
+ttt=('same syntax in power Bi')
+print(ttt)
+input('press enter')
+
+
+uuu=('same syntax in python')
+print(uuu)
+input('press enter')
+
+
+vvv=('know the functions or relearn the syntax')
+print(vvv)
+input('press enter')
+
+www=('information RECALL does help the brain past info and current info')
+print(www)
+input('press enter')
+
+
+xxx=('also create new insights.')
+print(xxx)
+input('press enter')
+
+yyy=('the back yard rose 2015--tick')
+print(yyy)
+input('press enter')
+
+
+zzz=('goggle yard 2015-to 2011-- tick')
+print(zzz)
+input('press enter')
+
+aaaa=('INTERLEAVING and the SPACE-ING')
+print(aaaa)
+input('press enter')
+
+bbbb=('understand the concepts,')
+print(bbbb)
+input('print enter')
+
+
+cccc=('dont wory about the timeframe or future projects just MASTER the skills..')
+print(cccc)
+input('press enter')
+
+
+dddd=('end of transmission')
+print(dddd)
+input('press enter')
+
